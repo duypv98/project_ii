cb8269c707ffca2bdd0b99f7e6e097141f0a4ee9
duypv98 <duypv98@gmail.com>
2019-05-12 23:38:16 +0700

    db

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,16 @@
 'use strict'
+import mysql from 'mysql';
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'admin123',
+    database: process.env.DB_NAME || 'project_ii'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('DATABASE OK!');
+});
+
+module.exports = db;
