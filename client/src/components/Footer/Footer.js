0ae6b3abdd6d5d13b120f50a1d56beb344fb3442
duypv98 <duypv98@gmail.com>
2019-05-27 19:52:01 +0700

    Components

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,2 +1,39 @@
 import React, { Component } from 'react';
 import './footer.css';
+import { Fa } from 'mdbreact';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="row footer-row-1">
+                    <div className="col-md-3">
+                        <img className="footer-logo" src={require('../../assets/images/React-Logo-Trans.png')}></img>
+                    </div>
+                    <div className="col-md-3">
+                        <h5>GoPro-MDb</h5>
+                        <p>Movies - Cinemas VN Forums</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h5>Development Stack</h5>
+                        <ul>
+                            <li>ReactJS</li>
+                            <li>Node.js - ExpressJS</li>
+                            <li>MySQL</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <h5>Sponsored</h5>
+                        <ul>
+                            <li>GoPro Teams</li>
+                            <li>HUST</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row footer-row-2">
+                    <h6>Copyright © 2019 GoPro Teams</h6>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './navbar.css';
+import {
+    Navbar, NavbarBrand, NavbarNav, NavbarToggler,
+    Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem
+} from 'mdbreact';
+
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+            value: ""
+        };
+        this.onClick = this.onClick.bind(this);
+        this.handleSubmit = this.props.handleSubmit;
+    }
+
+    onClick() {
+        this.setState({
+            collapse: !this.state.collapse
+        });
+    }
+
+    onSubmit = (e) => {
+        if (e.key === 'Enter') {
+            this.handleSubmit(this.state.value)
+        }
+    }
+
+    render() {
+        return (
+            <Navbar color="warning-color" light expand="md" scrolling>
+                <NavbarBrand href="/">
+                    <img className="brand-img" src={require('./../../assets/images/React-Logo-Trans.png')} alt=""></img>
+                </NavbarBrand>
+                <NavLink to="/" className="nav-title">GoPro-MDb</NavLink>
+                {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+                <Collapse isOpen={this.state.collapse} navbar>
+                    <NavbarNav left>
+                        <NavItem active>
+                            <NavLink to="/" className="nav-custom">Home Page</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <Dropdown>
+                                <DropdownToggle nav caret className="nav-custom">Thể loại</DropdownToggle>
+                                <DropdownMenu className="drop-menu-custom">
+                                    <DropdownItem href="type/Action">Hành động</DropdownItem>
+                                    <DropdownItem href="type/Adventure">Phiêu lưu</DropdownItem>
+                                    <DropdownItem href="type/Fiction">Viễn tưởng</DropdownItem>
+                                    <DropdownItem href="type/Horror">Kinh dị</DropdownItem>
+                                    <DropdownItem href="type/Family">Gia đình</DropdownItem>
+                                    <DropdownItem href="type/Comedy">Hài hước</DropdownItem>
+                                    <DropdownItem href="type/Romance">Tình cảm</DropdownItem>
+                                    <DropdownItem href="type/Drama">Tâm lý</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/login" className="nav-custom">Quản lý</NavLink>
+                        </NavItem>
+                    </NavbarNav>
+                    <NavbarNav right>
+                        <NavItem>
+                            <input className="form-control" type="text" placeholder="Tìm kiếm phim..."
+                                aria-label="Tìm tên phim"
+                                onChange={e => this.setState({ value: e.target.value })}
+                                onKeyPress={e => { this.onSubmit(e) }} />
+                        </NavItem>
+                    </NavbarNav>
+                </Collapse>
+            </Navbar>
+        )
+    }
+}
